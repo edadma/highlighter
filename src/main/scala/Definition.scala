6a106819741f28b5dba144e59b2741d6654be995
Edward A Maxedon <edadma@gmail.com>
2018-06-23 14:24:57 -0400

    start coding Highlighter

--- src/main/scala/Definition.scala
+++ src/main/scala/Definition.scala
@@ -1,6 +1,6 @@
 package xyz.hyperreal.highlighter
 
-import scala.util.matching.Regex
+import java.util.regex.{Matcher, Pattern}
 
 import xyz.hyperreal.backslash.AST
 
@@ -8,6 +8,7 @@ import xyz.hyperreal.backslash.AST
 case class Definition( sections: Seq[Section] )
 
 trait Section
+case class Name( s: String ) extends Section
 case class Options( options: Seq[LexerOption] ) extends Section
 case class Templates( templates: Map[String, AST] ) extends Section
 case class States( states: Seq[State] ) extends Section
@@ -21,14 +22,15 @@ case object Ignorecase extends LexerOption
 case object Dotall extends LexerOption
 
 trait Rule
-case class MatchRule( regex: Regex, actions: Seq[Action] ) extends Rule
-case class MismatchRule( regex: Regex, actions: Seq[Action] ) extends Rule
-case class FailRule( actions: Seq[Action] ) extends Rule
-case class IncludeRule( include: String ) extends Rule { val state = new Const[Seq[Rule]] }
+case class MatchRule( regex: Pattern, actions: Seq[Action] ) extends Rule
+case class MismatchRule( regex: Pattern, actions: Seq[Action] ) extends Rule
+case class DefaultRule(actions: Seq[Action] ) extends Rule
+case class IncludeRule( include: String ) extends Rule { val rules = new Const[Seq[Rule]] }
 
 trait Action
 case class Match( tok: Token ) extends Action { val ast = new Const[AST] }
 case class Groups( toks: Seq[Token] ) extends Action { val asts = Seq.fill( toks.length )( new Const[AST] ) }
+case class Using( fs: Seq[Either[Token, Highlighter]] ) extends Action
 case class Push( name: String ) extends Action { val state = new Const[State] }
 case object Pop extends Action
 case class Popn( n: Int ) extends Action
---
+++ src/main/scala/Highlighter.scala
@@ -0,0 +1,167 @@
+//@
+package xyz.hyperreal.highlighter
+
+import java.util.regex.Pattern
+
+import scala.util.matching.Regex
+import scala.collection.mutable.ArrayStack
+import xyz.hyperreal.backslash.{AST, Command, Parser, Renderer}
+
+
+abstract class Highlighter {
+
+  def define: Definition
+
+  var trace = false
+  val parser = new Parser( Command.standard )
+  val (name, templates, states, classes, includes) = {
+    var flags = 0
+    var _name: String = getClass.getName
+    var _templates: Map[String, AST] = null
+    var _states: Map[String, State] = null
+    var _classes: Map[String, String] = Map()
+    var _includes: Map[String, Seq[Rule]] = Map()
+
+    define match {
+      case Definition( sections ) =>
+        sections foreach {
+          case Name( s ) => _name = s
+          case Options( options ) =>
+            options foreach {
+              case Ignorecase => flags |= Pattern.CASE_INSENSITIVE
+              case Dotall => flags |= Pattern.DOTALL
+            }
+          case Templates( templates ) => _templates = templates
+          case States( states ) => _states = states map (s => (s.name, s)) toMap
+          case Includes( includes ) => _includes = includes
+          case Classes( classes ) => _classes = classes
+        }
+    }
+
+    if (_templates eq null)
+      sys.error( "missing template section" )
+
+    if (!_templates.contains( "default" ))
+      sys.error( "missing default template" )
+
+    if (_states eq null)
+      sys.error( "missing states section" )
+
+    if (!_states.contains( "root" ))
+      sys.error( "missing root state" )
+
+    (_name, _templates, _states, _classes, _includes) }
+  val config =
+    Map(
+      "today" -> "MMMM d, y",
+      "include" -> ".",
+      "rounding" -> "HALF_EVEN"
+    )
+  val renderer = new Renderer( parser, config )
+
+  def highlight( code: String ) = {
+
+    val stack =
+      new ArrayStack[State] {
+        push( states("root") )
+      }
+    val result = new StringBuilder
+    val chunk = new StringBuilder
+    var prevclass: String = null
+    var prevast: AST = null
+
+    def dotrace( op: String, s: => Any ) =
+      if (trace)
+        println( s"$op: $s" )
+
+    def output( s: String, clas: Token ) =
+      if (s nonEmpty) {
+        dotrace( "output",  s""""$s", $clas""" )
+
+        val (cls, tmp) =
+          clas match {
+            case Token( c, t ) => (c, t)
+          }
+
+        templates get tmp match {
+          case None =>
+            if (tmp == "text") {
+              flush
+              result ++= s
+            } else
+              sys.error( s"unrecognized template: $tmp" )
+          case Some( ast ) =>
+            if (cls == prevclass)
+              chunk ++= s
+            else {
+              flush
+              prevast = ast
+              prevclass = cls
+              chunk ++= s
+            }
+        }
+      }
+
+    def flush: Unit =
+      if (prevclass ne null) {
+        result ++=
+          renderer.capture( prevast, Map("class" -> classes.getOrElse(prevclass, prevclass), "text" -> chunk.toString) )
+        chunk.clear
+        prevclass = null
+      }
+
+    def text( s: String ): Unit = output( s, "text/text" )
+
+    def highlight( pos: Int ): Unit = {
+      def prefix( regex: Pattern ) = {
+        val m = regex matcher code
+
+        m.region( pos, code.length )
+        if (m.lookingAt) Some( m ) else None
+      }
+
+      def search[A, B]( seq: Seq[A], f: A => Option[B] ): Option[B] = {
+        val it = seq.iterator
+
+        while (it.hasNext)
+          f( it.next ) match {
+            case None =>
+            case res => return res
+          }
+
+        None
+      }
+
+      case class Match( start: Int, end: Int, starts: Seq[Int], ends: Seq[Int] )
+
+      def apply( rule: Rule ): Option[Seq[(Option[Match], Action)]] =
+        rule match {
+          case MatchRule( regex, actions ) =>
+            prefix( regex ) map { m =>
+                // create Match
+              }
+          case MismatchRule( regex, actions ) => option( !prefix(regex), actions )
+          case DefaultRule( actions ) => Some( actions )
+          case rule@IncludeRule( include ) =>
+            search( rule.rules(
+              includes get include match {
+                case None => sys.error( s"unknown include: $include" )
+                case Some( r ) => r
+              } ), apply )
+        }
+
+      search( stack.top.rules, apply ) match {
+        case None =>
+        case Some( actions ) =>
+          actions foreach {
+            case Match( tok ) =>
+
+          }
+      }
+
+
+    }
+
+  }
+
+}
