4415ad91715f7635abadc2bbbc7928dd97f007bf
Edward A Maxedon <edadma@gmail.com>
2018-06-26 14:18:16 -0400

    approximate github html highlighting

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -34,31 +34,25 @@ object Main extends App {
           |states
           |  root:
           |    &\S*?;     => entity
-          |    \<\!\[CDATA\[.*?\]\]\>     => preproc
-          |    <!--      => comment >comment
-          |    <\?.*?\?>  => preproc
-          |    <![^>]*> => preproc
-          |    (<)\s*(script)\s* => (punct tag) >script-content >tag
-          |    (<)\s*(style)\s* => (punct tag) >style-content >tag
-          |    (<)\s*([\w:.-]+) => (punct tag) >tag
-          |    (<)\s*(/)\s*([\w:.-]+)\s*(>) => (punct punct tag punct)
+          |    <!--      => pl-c >comment
+          |    <\s*(script)\s* => (pl-ent) >script-content >tag
+          |    <\s*(style)\s* => (pl-ent) >style-content >tag
+          |    <\s*([\w:.-]+) => (pl-ent) >tag
+          |    <\s*/\s*([\w:.-]+)\s*> => (pl-ent)
           |  comment:
-          |    [^-]+   => comment
-          |    -->   => comment ^
-          |    -   => comment
+          |    [^-]+   => pl-c
+          |    -->   => pl-c ^
+          |    -   => pl-c
           |  tag:
-          |    ([\w:-]+)\s*(=)\s* => (attr oper) >attr
-          |    [\w:-]+ => attr
-          |    (/?)\s*(>) => (punct punct) ^
-          |  script-content:
-          |    (<)\s*(/)\s*(script)\s*(>) => (punct punct tag punct) ^
-          |    .+?(?=<\s*/\s*script\s*>) => using-javascript
+          |    ([\w:-]+)\s*=\s* => (pl-e) >attr
+          |    [\w:-]+ => pl-e
+          |    /?\s*> => text ^
           |  style-content:
-          |    (<)\s*(/)\s*(style)\s*(>) => (punct punct tag punct) ^
-          |    .+?(?=<\s*/\s*style\s*>) => using-css
+          |    <\s*/\s*(style)\s*> => (pl-ent) ^
+          |    .+?(?=<\s*/\s*style\s*>) => pl-s1
           |  attr:
-          |    ".*?" => string ^
-          |    '.*?' => string ^
+          |    (")(.*?)(") => (pl-pds pl-s pl-pds) ^
+          |    (')(.*?)(') => (pl-pds pl-s pl-pds) ^
           |    [^\s>]+ => string ^
         """.stripMargin
       )
@@ -69,3 +63,21 @@ object Main extends App {
   println( highlighter.highlight(input) )
 
 }
+
+/*
+
+<div class="highlight highlight-text-html-basic"><pre>&lt;!DOCTYPE html&gt;
+&lt;<span class="pl-ent">html</span>&gt;
+  &lt;<span class="pl-ent">head</span>&gt;
+    &lt;<span class="pl-ent">style</span>&gt;<span class="pl-s1"></span>
+<span class="pl-s1">      <span class="pl-ent">body</span> {<span class="pl-c1"><span class="pl-c1">background-color</span></span>: <span class="pl-c1">powderblue</span>;}</span>
+<span class="pl-s1">      <span class="pl-ent">p</span>    {<span class="pl-c1"><span class="pl-c1">color</span></span>: <span class="pl-c1">red</span>;}</span>
+<span class="pl-s1">    </span><span class="pl-s1">&lt;</span>/<span class="pl-ent">style</span>&gt;
+  &lt;/<span class="pl-ent">head</span>&gt;
+  &lt;<span class="pl-ent">body</span>&gt;
+    <span class="pl-c"><span class="pl-c">&lt;!--</span> comment <span class="pl-c">--&gt;</span></span>
+    &lt;<span class="pl-ent">p</span> <span class="pl-e">align</span>=<span class="pl-s"><span class="pl-pds">"</span>right<span class="pl-pds">"</span></span>&gt;paragraph&lt;/<span class="pl-ent">p</span>&gt;
+  &lt;/<span class="pl-ent">body</span>&gt;
+&lt;/<span class="pl-ent">html</span>&gt;</pre></div>
+
+ */
