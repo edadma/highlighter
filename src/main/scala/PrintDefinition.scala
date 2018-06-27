8e50f1d5551107e4b6aaf1ae30eda727c8ab0140
Edward A Maxedon <edadma@gmail.com>
2018-06-27 00:50:02 -0400

    cont coding pretty printer

--- src/main/scala/PrintDefinition.scala
+++ src/main/scala/PrintDefinition.scala
@@ -1,8 +1,28 @@
 package xyz.hyperreal.highlighter
 
+import java.util.regex.Pattern
+
 
 object PrintDefinition extends App {
 
-  HighlighterParser( io.Source.stdin )
+  HighlighterParser( io.Source.stdin ) match {
+    case Definition( sections ) =>
+      sections foreach {
+        case InfoItems( items ) =>
+          items foreach {
+            case Name( s ) => _name = s
+          }
+        case Options( options ) =>
+          options foreach {
+            case Ignorecase => _flags |= Pattern.CASE_INSENSITIVE
+            case Dotall => _flags |= Pattern.DOTALL
+            case Multiline => _flags |= Pattern.MULTILINE
+          }
+        case Templates( templates ) => _templates = templates
+        case States( states ) => _states = states map (s => (s.name, s)) toMap
+        case Includes( includes ) => _includes = includes
+        case Classes( classes ) => _classes = classes
+      }
+  }
 
 }
