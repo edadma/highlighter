5d3e4224567dbd85e27f30ad6f40f70965e4b42d
Edward A Maxedon <edadma@gmail.com>
2018-07-03 14:13:14 -0400

    example

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -27,47 +27,7 @@ object Main extends App {
         override def define: Definition = HighlighterParser( io.Source.fromFile("highlighters/css.hl") )
       })
 
-      def define = HighlighterParser(
-        """
-          |highlighter
-          |  name: HTML
-          |options
-          |  regex: dotall ignorecase
-          |templates
-          |  default: {{ <span class="\class">\escape\text</span> }}
-          |states
-          |  root:
-          |    &\S*?;                        => Name.Entity
-          |    \<\!\[CDATA\[.*?\]\]\>        => Comment.Preproc
-          |    <!--                          => Comment >comment
-          |    <\?.*?\?>                     => Comment.Preproc
-          |    <![^>]*>                      => Comment.Preproc
-          |    (<)\s*(script)\s*             => (Punctuation Name.Tag) >script-content >tag
-          |    (<)\s*(style)\s*              => (Punctuation Name.Tag) >style-content >tag
-          |    (<)\s*([\w:.-]+)              => (Punctuation Name.Tag) >tag
-          |    (<)\s*(/)\s*([\w:.-]+)\s*(>)  => (Punctuation Punctuation Name.Tag Punctuation)
-          |  comment:
-          |    [^-]+  => Comment
-          |    -->    => Comment ^
-          |    -      => Comment
-          |  tag:
-          |    ([\w:-]+)\s*(=)\s*  => (Name.Attribute Operator) >attr
-          |    [\w:-]+             => Name.Attribute
-          |    (/?)\s*(>)          => (Punctuation Punctuation) ^
-          |  script-content:
-          |    (<)\s*(/)\s*(script)\s*(>)  => (Punctuation Punctuation Name.Tag Punctuation) ^
-          |    .+?(?=<\s*/\s*script\s*>)   => [javascript]
-          |  style-content:
-          |    (<)\s*(/)\s*(style)\s*(>)  => (Punctuation Punctuation Name.Tag Punctuation) ^
-          |    .+?(?=<\s*/\s*style\s*>)   => [css]
-          |  attr:
-          |    ".*?"    => String ^
-          |    '.*?'    => String ^
-          |    [^\s>]+  => String ^
-        """.stripMargin
-      )
-
-      println( define )
+      def define = HighlighterParser( io.Source.fromFile("highlighters/html.hl") )
     }
 
   println( highlighter.highlight(input) )
