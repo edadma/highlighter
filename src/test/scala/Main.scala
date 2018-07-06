445ecdf719e0888baad3399e69105667d7a94b7a
Edward A Maxedon <edadma@gmail.com>
2018-07-05 23:38:17 -0400

    cont coding backslash highlighter

--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -3,11 +3,18 @@ package xyz.hyperreal.highlighter
 
 object Main extends App {
 
-  val input = io.Source.fromFile( "test.html" ) mkString
-  val highlighter = highlighters.HTMLHighlighter
+//  val input = io.Source.fromFile( "test.html" ) mkString
+  val input =
+    """
+      |\asdf
+    """.stripMargin
+  val highlighter =
+    new Highlighter {
+      def define = HighlighterParser( io.Source.fromFile("highlighters/backslash.hl") )
+    }
 
-  Console.withOut( new java.io.FileOutputStream("htest1.html") ) {
+//  Console.withOut( new java.io.FileOutputStream("htest1.html") ) {
     println( highlighter.highlight(input) )
-  }
+//  }
 
 }
